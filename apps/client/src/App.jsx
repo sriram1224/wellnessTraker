b96e3756e5a9e4e31539cd16be4d12fa4e218f27
Bhargav Sri Ram <kasukurthibhargav@gmail.com>
2025-08-25 02:14:22 +0530

    Update client application

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -1,12 +1,5 @@
-import "./App.css";
-
-function App() {
+export default function App() {
   return (
-    <div>
-      <h1 className="text-xl font-bold text-red-600">Wellness Tracker</h1>
-      <p className="text-lg">Track your wellness journey</p>
-    </div>
+    <h1 className="text-3xl font-bold underline text-red-600">Hello world!</h1>
   );
 }
-
-export default App;
